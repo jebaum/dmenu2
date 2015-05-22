61c6addd621cd0c47c25bd610c45ebda21c24ddc
James Baumgarten <jebaum@ucla.edu>
2015-05-21 22:05:50 -0700

    add script for easy line by line profiling

---
+++ profile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+make clean
+make debug
+if [ "$#" != "0" ]; then
+    "$@"
+else
+    if hash locate 2>/dev/null; then
+        locate "" | ./dmenu -z -s -centerx -centery -w 500 -l 20
+    else
+        ls -R /bin | ./dmenu -z -s -centerx -centery -w 500 -l 20
+    fi
+fi
+
+
+gprof -l ./dmenu gmon.out > gprof_latest
+rm gmon.out
+less gprof_latest
